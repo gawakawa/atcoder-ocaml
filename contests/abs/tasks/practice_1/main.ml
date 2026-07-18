81c6998147f78cb816af80496183f4e0fb2f185f
gawakawa <mashurakiryu@icloud.com>
2026-07-18 11:37:27 +0900

    :sparkles: Implement practice_1 solution

--- contests/abs/tasks/practice_1/main.ml
+++ contests/abs/tasks/practice_1/main.ml
@@ -1,1 +1,4 @@
-let () = ()
+let a = Scanf.sscanf (read_line ()) " %d" Fun.id
+let sum_bc = Scanf.sscanf (read_line ()) " %d %d" (fun b c -> b + c)
+let s = Scanf.sscanf (read_line ()) " %s" Fun.id
+let () = Printf.printf "%d %s\n" (a + sum_bc) s
