869ae2b9c11b2fd800510eec046fbd462dfcb98a
gawakawa <mashurakiryu@icloud.com>
2026-07-18 19:02:17 +0900

    :sparkles: Implement abc086_a solution

--- contests/abs/tasks/abc086_a/main.ml
+++ contests/abs/tasks/abc086_a/main.ml
@@ -1,1 +1,4 @@
-let () = ()
+let () =
+  let a, b = Scanf.sscanf (read_line ()) " %d %d" (fun a b -> (a, b)) in
+  let ans = if a * b mod 2 == 1 then "Odd" else "Even" in
+  Printf.printf "%s\n" ans
