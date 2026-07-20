2a2821effbdf329a9bf8e79a46ab878fe0ac84a6
gawakawa <mashurakiryu@icloud.com>
2026-07-20 23:58:13 +0900

    :sparkles: Solve abc088_b

--- contests/abs/tasks/abc088_b/main.ml
+++ contests/abs/tasks/abc088_b/main.ml
@@ -1,1 +1,22 @@
-let () = ()
+open Printf
+
+let partitioni p lst =
+  List.mapi (fun i x -> i, x) lst
+  |> List.partition (fun (i, _) -> p i)
+  |> fun (yes, no) -> List.map snd yes, List.map snd no
+;;
+
+let sum = List.fold_left ( + ) 0
+
+let solve lst =
+  List.sort (fun a b -> compare b a) lst
+  |> partitioni (fun i -> i mod 2 = 0)
+  |> fun (evens, odds) -> sum evens - sum odds
+;;
+
+let () =
+  let _n = read_int () in
+  let a_lst = read_line () |> String.split_on_char ' ' |> List.map int_of_string in
+  let ans = solve a_lst in
+  printf "%d\n" ans
+;;
