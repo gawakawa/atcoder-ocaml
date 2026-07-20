b96a96074e60553cdcae8a026ec0394f7fcc996b
gawakawa <mashurakiryu@icloud.com>
2026-07-21 00:58:06 +0900

    :sparkles: Solve abc085_b

--- contests/abs/tasks/abc085_b/main.ml
+++ contests/abs/tasks/abc085_b/main.ml
@@ -1,1 +1,10 @@
-let () = ()
+open Printf
+
+let solve lst = List.sort_uniq (fun a b -> compare b a) lst |> List.length
+
+let () =
+  let n = read_int () in
+  let d_lst = List.init n (fun _ -> read_int ()) in
+  let ans = solve d_lst in
+  printf "%d\n" ans
+;;
