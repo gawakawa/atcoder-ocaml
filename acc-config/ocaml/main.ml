eb7c3d202dbb439c491dfc4ad766cef22b82b4bd
gawakawa <mashurakiryu@icloud.com>
2026-08-05 00:30:46 +0900

    :sparkles: Add build_directed_graph/build_undirected_graph to ocaml template

--- acc-config/ocaml/main.ml
+++ acc-config/ocaml/main.ml
@@ -94,4 +94,28 @@ let[@warning "-32"] rec combinations k lst : _ Iter.t =
       without_x yield)
 ;;
 
+(** 有向グラフを構築する
+
+    @param n 頂点数
+    @param edges 有向辺 (u, v) のリスト
+    @return 隣接リスト表現の有向グラフ *)
+let[@warning "-32"] build_directed_graph n edges =
+  let g = Array.create ~len:n [] in
+  List.iter edges ~f:(fun (u, v) -> g.(u) <- v :: g.(u));
+  g
+;;
+
+(** 無向グラフを構築する
+
+    @param n 頂点数
+    @param edges 無向辺 (u, v) のリスト
+    @return 隣接リスト表現の無向グラフ *)
+let[@warning "-32"] build_undirected_graph n edges =
+  let g = Array.create ~len:n [] in
+  List.iter edges ~f:(fun (u, v) ->
+    g.(u) <- v :: g.(u);
+    g.(v) <- u :: g.(v));
+  g
+;;
+
 let () = ()
